13408d15c2b02a404869d005a88c3a472c50b5b1
uriel levy <uriellevy4892@gmail.com>
2021-11-04 13:17:50 +0200

    get & post routes added

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,20 +1,55 @@
+const Transaction = require("../models/Transaction");
+
 //@desc GET all transactions
 //@route GET /api/v1/transactions
 //@access Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 //@desc add all transactions
 //@route POST /api/v1/transactions
 //@access Public
-exports.addTransactions = (req, res, next) => {
-  res.send("POST transactions");
+exports.addTransactions = async (req, res, next) => {
+  try {
+    const { text, amount } = req.body;
+    const transaction = await Transaction.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      data: transaction,
+    });
+  } catch (error) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      return res.status(400).json({
+        success: false,
+        error: messages,
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        error: "Server Error",
+      });
+    }
+  }
 };
 
 //@desc Delete all transactions
 //@route DELETE /api/v1/transactions/:id
 //@access Public
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions = async (req, res, next) => {
   res.send("DELETE transactions");
 };
