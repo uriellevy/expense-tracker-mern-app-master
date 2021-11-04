39505249fd7730ebeb6f3c8bf3595c81d742b50a
uriel levy <uriellevy4892@gmail.com>
2021-11-04 13:25:55 +0200

    delete route added

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -51,5 +51,23 @@ exports.addTransactions = async (req, res, next) => {
 //@route DELETE /api/v1/transactions/:id
 //@access Public
 exports.deleteTransactions = async (req, res, next) => {
-  res.send("DELETE transactions");
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: "No transaction found",
+      });
+    }
+    await transaction.remove();
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
