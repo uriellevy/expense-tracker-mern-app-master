bef28201741d5b6657f224cf0566b73889cd7e66
uriel levy <uriellevy4892@gmail.com>
2021-11-04 12:41:37 +0200

    connected to mongoose data-base

---
+++ server/controllers/transactions.js
@@ -0,0 +1,20 @@
+//@desc GET all transactions
+//@route GET /api/v1/transactions
+//@access Public
+exports.getTransactions = (req, res, next) => {
+  res.send("GET transactions");
+};
+
+//@desc add all transactions
+//@route POST /api/v1/transactions
+//@access Public
+exports.addTransactions = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+//@desc Delete all transactions
+//@route DELETE /api/v1/transactions/:id
+//@access Public
+exports.deleteTransactions = (req, res, next) => {
+  res.send("DELETE transactions");
+};
--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
-
+const connectDB = require("./config/db");
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const transactions = require("./routes/transactions");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("hello"));
+app.use("/api/v1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
