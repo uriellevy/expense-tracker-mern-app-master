03a6a16569c568918faea07835e139a96ee209cb
uriel levy <uriellevy4892@gmail.com>
2021-11-04 09:44:37 +0200

    test

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,18 +4,16 @@ import { GlobalContext } from "../context/GlobalState";
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
-
   const { addTransaction } = useContext(GlobalContext);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
+      id: Math.floor(Math.random() * 100000),
       text,
       amount: +amount,
     };
-
     addTransaction(newTransaction);
   };
 
@@ -27,21 +25,20 @@ export const AddTransaction = () => {
           <label htmlFor="text">Text</label>
           <input
             type="text"
+            placeholder="Enter text..."
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
           />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
+            Amount <br /> (negative - expense, positive - income)
           </label>
           <input
             type="number"
+            placeholder="Enter amount..."
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
           />
         </div>
         <button className="btn">Add transaction</button>
