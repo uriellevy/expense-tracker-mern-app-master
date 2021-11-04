1210b156fa5360353418c6d74f828941e4337f8c
uriel levy <uriellevy4892@gmail.com>
2021-11-04 10:46:41 +0200

    open server

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,7 +3,9 @@ import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
+
   const amounts = transactions.map((transaction) => transaction.amount);
+  // console.log(amounts);
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <>
